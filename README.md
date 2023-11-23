13d1015a5108ebad624656545822033780b131f5
Sasha <sasazd@hotmail.com>
2023-11-23 14:48:56 +0000

    while loop added

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 This script has been written for ubuntu server 23.10 on Raspberry Pi 5, since to date 2023/11/16 the new fan header is unsupported on this specific OS making the fan go to 100% speed all the time.
 
+while loop has been added, thanks to https://www.youtube.com/watch?v=M--nU_Zg-ZQ
+
 ## Installation
 
 - clone the project
@@ -9,8 +11,6 @@ This script has been written for ubuntu server 23.10 on Raspberry Pi 5, since to
 - give it execution permission with  
    `sudo chmod +x ./fan_script.sh`
 - with root priviledge (`sudo su`), run `crontab -e`
-- add this line to run it every minute:  
-  `* * * * * /<path_to_script>/fan_script.sh`
 - save and close, the script should kick in.
 
 This repository will be archived the moment a proper driver will be released on the ubuntu kernel, this is only a temporary fix.
--- fan_script.sh
+++ fan_script.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
+while true; do
+    temp=$(/usr/bin/vcgencmd measure_temp | egrep -o '[0-9]*\.[0-9]*')
+    echo $temp
 
-temp=$(/usr/bin/vcgencmd measure_temp | egrep -o '[0-9]*\.[0-9]*')
+    if (( $(echo "$temp < 55" | bc -l) )); then
+        temp=0
+    elif (( $(echo "$temp < 60" | bc -l) )); then
+        temp=1
+    elif (( $(echo "$temp < 68" | bc -l) )); then
+        temp=2
+    elif (( $(echo "$temp < 75" | bc -l) )); then
+        temp=3
+    else
+        temp=4
+    fi
 
-if (( $(echo "$temp < 55" | bc -l) )); then
-    temp=0
-elif (( $(echo "$temp < 60" | bc -l) )); then
-    temp=1
-elif (( $(echo "$temp < 68" | bc -l) )); then
-    temp=2
-elif (( $(echo "$temp < 75" | bc -l) )); then
-    temp=3
-else
-    temp=4
-fi
-
-echo $temp > /sys/class/thermal/cooling_device0/cur_state
+    echo $temp > /sys/class/thermal/cooling_device0/cur_state
+    sleep 10;
+done
